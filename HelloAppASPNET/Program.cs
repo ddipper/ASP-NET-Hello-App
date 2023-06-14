127aaeb761948a17466400ecca45ad7bc9bd4ac6
Dmitry <dimonbyt@gmail.com>
2023-06-14 15:41:22 +0300

    add some code.

--- HelloAppASPNET/Program.cs
+++ HelloAppASPNET/Program.cs
@@ -1,12 +1,124 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
 
+    if(context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string name = form["name"];
+        string age = form["age"];
+        string[] langs = form["langs"];
+        string langList = "";
+
+        foreach(var l in langs)
+        {
+            langList += $" {l}";
+        }
+
+        await context.Response.WriteAsync($"<div><p>Name: {name}</p>" + 
+            $"<p>Age: {age}</p>" + 
+            $"<div>Languages: {langList}</div></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+});
 
 app.Run();
 
 
+
+/*app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+
+    if (context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string name = form["name"];
+        string age = form["age"];
+        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+});*/
+
+
+/*app.Run(async (context) =>
+{
+    var fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+    var fileInfo = fileProvider.GetFileInfo("Night.png");
+
+    context.Response.Headers.ContentDisposition = "attachment; filename=ddipperNight.png";
+    await context.Response.SendFileAsync(fileInfo);
+});*/
+
+
+/*app.Run(async (context) =>
+{
+    var path = context.Request.Path;
+    var fullPath = $"html/{path}";
+    var response = context.Response;
+
+    response.ContentType = "text/html; charser=utf-8";
+    if(File.Exists(fullPath))
+    {
+        await response.SendFileAsync(fullPath);
+    }
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsync("<h2>Not Found</h2>");
+    }
+
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("html/index.html");
+});*/
+
+
+//app.Run(async (context) => await context.Response.SendFileAsync("Night.png"));
+
+
+/*app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html";
+    var stringBuilder = new System.Text.StringBuilder("<h3>QueryString parametrs</h3><table>");
+    stringBuilder.Append("<tr><td>Parametr</td><td>Value</td></tr>");
+
+    foreach(var param in context.Request.Query)
+    {
+        stringBuilder.Append($"<tr><td>{param.Key}</td><td>{param.Value}</td></tr>");
+    }
+
+    stringBuilder.Append("</table>");
+
+    string name = context.Request.Query["name"];
+    string age = context.Request.Query["age"];
+
+    await context.Response.WriteAsync(stringBuilder.ToString() + $"\n {name}: {age}");
+});*/
+
+
 //app.Run(async (context) => await context.Response.WriteAsync("Welcome at suicide world"));
+
+
+/*var path = context.Request.Path;
+var now = DateTime.Now;
+var response = context.Response;
+
+if (path == "/date")
+    await response.WriteAsync($"Date: {now.ToShortDateString()}");
+if (path == "/time")
+    await response.WriteAsync($"Time: {now.ToShortTimeString()}");*/
 
 
 /*app.Run(async (context) =>
@@ -18,6 +130,7 @@ app.Run();
     response.Headers.Append("secret-id", "256");
     await context.Response.WriteAsync("Welcome at this suicide world");
 });*/
+
 
 /*app.Run(async (context) =>
 {
